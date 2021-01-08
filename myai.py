9cb4665054a84e47c72e8aca18ddce012bab234f
Emmanuel Jones <manny1206@hotmail.com>
2021-01-08 15:55:13 -0500

    work on AI q learning

--- myai.py
+++ myai.py
@@ -11,16 +11,17 @@ import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
 
-# architecture of the neural network
+###### Architecture of the neural network ######
 class Network(nn.Module):
     
     def __init__(self, input_size, output_size):
         super(Network, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
-        
+
+        # full connection from input to hidden layer
         self.fc1 = nn.Linear(input_size, 30)
-        
+        # full connection from hidden layer to output
         self.fc2 = nn.Linear(30, output_size)
         
     def prop_forward(self, state):
@@ -29,9 +30,34 @@ class Network(nn.Module):
         
         return q_vals
 
-# Experience Replay...
+###### Experience Replay... ######
 # consider the past n = 100 remembered states to decide an action
 class ReplayMemory(object):
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = [] # memory for past states
+
+    # function to push an event to the replay memory
+    def push(self, event):
+        self.memory.append(event)
+        # make sure the memory doesn't exceed the capacity
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    
+    # function to randomly sample from the memory
+    def random_sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+###### Implement Deep Q-Learning ######
+
+class Dqn():
+    def __init__(self, input_size, output_size, gamma):
+        self.gamma = gamma
+        self.reward_window =  []
+        self.model = Network(input_size, output_size)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(),lr = 0.001)
+        self.prev_state = torch.Tensor(input_size).unsqueeze(0)
+        self.prev_output = 0
+        self.prev_reward = 0
